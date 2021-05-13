5a84772bed1bc7ed3b9182884f2b44540276c1b4
Randall Arms <contact.randallArms@gmail.com>
2021-05-13 09:42:10 -0700

    v0.8.1 | Added 1914 names
    * Added most popular baby names for 1914 from the SSA website.

--- objectify.py
+++ objectify.py
@@ -18,22 +18,22 @@ def objectify():
     years = [] 
     years_dict = {}
     contents = os.listdir(__names_location__)
-
+    
     # Find all year directories
     for item in contents:
         years.append(item)
         years_dict[item] = {}
-
+    
     # Add the available years to the nested dict of each gender in data
     data['male'] = years_dict.copy()
     data['female'] = years_dict.copy()
-
+    
     # Add each year's names to the data object
     for year in years:
         __year_location__ = os.path.join(__names_location__, year + "/")  
         __unsorted_location__ = os.path.join(__year_location__, "unsorted.txt")
         names = open(__unsorted_location__, "r")
-
+        
         # Fill list of possible names from file
         male_year_names = {}
         female_year_names = {}
